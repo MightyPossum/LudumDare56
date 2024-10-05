a28d74afc306d165b94fb05773a911605170c498
Ørjan Sømme <ranzor@pop-os.localdomain>
2024-10-05 21:30:43 +0200

    Shooting is a thing now

--- Assets/Scripts/enemy.gd
+++ Assets/Scripts/enemy.gd
@@ -12,6 +12,11 @@ var enemy_queue: Array = []
 var attack_target: String = "ally"
 
 @onready var health_bar: ProgressBar = $health_bar
+var has_been_damaged = false;
+var damage_timer = 0.5;
+var timer = 0.0;
+
+@export var attack_projectile: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,7 +25,16 @@ func _ready() -> void:
 	health_bar.visible = current_health < max_health
 	$AnimationPlayer.play("Idle")
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	
+	if has_been_damaged:
+		$AnimationPlayer.play("Hurt")
+		timer += delta
+		if timer >= damage_timer:
+			has_been_damaged = false
+			timer = 0.0
+		else:
+			$AnimationPlayer.play("Idle")
 	if enemy_queue.size() > 0 and not in_combat:
 		in_combat = true
 		attack()
@@ -44,8 +58,16 @@ func attack() -> void:
 	if enemy_queue.size() > 0:
 		var enemy = enemy_queue[0]
 		if enemy:
+			var bullet = attack_projectile.instantiate()
+			bullet.target_position = Vector2(enemy.position.x, enemy.position.y)
+			bullet.shooter = "Enemy"
+			get_parent().add_child(bullet)
+			bullet.position = global_position
+			bullet.attack_damage = attack_damage
+			bullet.attack_target = attack_target
+						
 			if enemy.has_method("take_damage"):
-				var is_enemy_alive = enemy.take_damage(attack_damage)
+				var is_enemy_alive = enemy.take_damage(0)
 				if not is_enemy_alive:
 					enemy_queue.erase(enemy)
 		else:
@@ -56,6 +78,7 @@ func take_damage(damage: int) -> bool:
 		current_health = max(current_health - damage, 0)
 		health_bar.value = current_health
 		health_bar.visible = current_health < max_health
+		has_been_damaged = true
 		
 	return is_alive
 
--- Assets/Scripts/player_creature.gd
+++ Assets/Scripts/player_creature.gd
@@ -21,6 +21,12 @@ var ongoing_wave : bool = false
 
 var movment_speed : int = 100
 
+var has_been_damaged = false;
+var damage_timer = 0.5;
+var timer = 0.0;
+
+@export var attack_projectile: PackedScene
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	health_bar.max_value = max_health
@@ -29,9 +35,25 @@ func _ready() -> void:
 	$Sprite2D/AnimationPlayer.play("Walk")
 
 func _physics_process(delta: float) -> void:
+	
+	if has_been_damaged:
+		$Sprite2D/AnimationPlayer.play("Hurt")
+		timer += delta
+		if timer >= damage_timer:
+			has_been_damaged = false
+			timer = 0.0
+	else:
+		$Sprite2D/AnimationPlayer.play("Walk")
+	
+	
 	if navgationAgent2D.is_target_reachable() and int(navgationAgent2D.distance_to_target() > GLOBALVARIABLES.game_manager.distance_to_enemy):
 		var next_location = navgationAgent2D.get_next_path_position()
 		var direction = global_position.direction_to(next_location)
+		if direction.x < 0:
+			$Sprite2D.flip_h = true
+		else:
+			$Sprite2D.flip_h = false
+			pass
 		global_position += direction * delta * movment_speed
 	elif navgationAgent2D.is_target_reachable() and enemy_queue.size() == 0:
 		pathing_initalized = false
@@ -63,8 +85,16 @@ func attack() -> void:
 	if enemy_queue.size() > 0:
 		var enemy = enemy_queue[0]
 		if enemy:
+			var bullet = attack_projectile.instantiate()
+			bullet.target_position = Vector2(enemy.position.x, enemy.position.y)
+			bullet.shooter = "Slime"
+			get_parent().add_child(bullet)
+			bullet.position = global_position
+			bullet.attack_damage = attack_damage
+			bullet.attack_target = attack_target
+						
 			if enemy.has_method("take_damage"):
-				var is_enemy_alive = enemy.take_damage(attack_damage)
+				var is_enemy_alive = enemy.take_damage(0)
 				if not is_enemy_alive:
 					enemy_queue.erase(enemy)
 		else:
@@ -75,6 +105,7 @@ func take_damage(damage: int) -> bool:
 		current_health = max(current_health - damage, 0)
 		health_bar.value = current_health
 		health_bar.visible = current_health < max_health
+		has_been_damaged = true
 		
 	return is_alive
 
