0ce2ca9e778690c385b97ad195e0bc2220184e69
Sindre Sømme <58266955+MightyPossum@users.noreply.github.com>
2024-10-05 17:23:44 +0200

    Starting waves (#10)
    * Instantiating enemies
    
    * changed spawn area
    
    * removed shit
    
    * Fixed pathing
    
    * Added spawning

--- Assets/Scripts/game_manager.gd
+++ Assets/Scripts/game_manager.gd
@@ -3,6 +3,8 @@ extends Node2D
 @export var distance_to_enemy = 20
 @onready var main_ui : Control = %MainUI
 
+var player_creature
+
 func _ready():
 	GLOBALVARIABLES.game_manager = self
 
@@ -14,8 +16,16 @@ func start_wave() -> void:
 	spawn_creatures()
 
 func spawn_creatures() -> void:
+	
+	player_creature = load("res://Assets/Scenes/player_creature.tscn")
+
 	for i in range(0,GLOBALVARIABLES.player_creature_count):
-		print(%spawnArea.shape.get_size())
-		#var top_left = %spawnArea.global_position
-		#var bottom_right = %spawnArea.global_position.x - %spawnArea.transform.
+		var area_middle = %spawnArea.global_position
+		var x_position = area_middle.x-%spawnArea.shape.get_size().x/2 + randi_range(1,%spawnArea.shape.get_size().x-1)
+		var y_position = area_middle.y-%spawnArea.shape.get_size().y/2 + randi_range(1,%spawnArea.shape.get_size().y-1)
 		
+		var spawn_vector = Vector2(x_position, y_position)
+		var spawn_creature = player_creature.instantiate()
+		spawn_creature.set_spawn_position(spawn_vector)
+		%Creatures.add_child(spawn_creature)
+		await get_tree().create_timer(0.25).timeout
