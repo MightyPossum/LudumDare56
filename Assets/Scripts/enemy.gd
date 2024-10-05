708140c44f9f78ea701635b455140816462c6f83
EyeLax <eyelax@outlook.com>
2024-10-05 13:50:22 +0200

    Npc logic working

--- Assets/Scripts/enemy.gd
+++ Assets/Scripts/enemy.gd
@@ -2,9 +2,9 @@ extends RigidBody2D
 
 const max_health: int = 100
 @export var current_health: int = max_health
-@export var attack_speed: float = .2
+@export var attack_speed: float = 1.0
 var in_combat: bool = false
-var is_alive: bool = true
+var is_alive: = true 
 
 @export var attack_damage: int = 10
 var enemy_queue: Array = []
@@ -12,13 +12,6 @@ var enemy_queue: Array = []
 @export_enum("ally", 'enemy') var attack_target: String
 
 @onready var health_bar: ProgressBar = $health_bar
-@onready var navgationAgent2D : NavigationAgent2D = get_node("NavigationAgent2D")
-@onready var game_manager : Node2D = get_parent().get_parent()
-
-var pathing_initalized : bool = false
-var targeted_enemy : bool = false
-
-var movment_speed : int = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -26,18 +19,7 @@ func _ready() -> void:
 	health_bar.value = current_health
 	health_bar.visible = current_health < max_health
 
-func _physics_process(delta: float) -> void:
-	if navgationAgent2D.is_target_reachable() and int(navgationAgent2D.distance_to_target() > game_manager.distance_to_enemy):
-		var next_location = navgationAgent2D.get_next_path_position()
-		var direction = global_position.direction_to(next_location)
-		global_position += direction * delta * movment_speed
-	elif navgationAgent2D.is_target_reachable() and enemy_queue.size() == 0:
-		pathing_initalized = false
-
-	if !pathing_initalized:
-		pathing_initalized = true
-		navgationAgent2D.set_target_position(game_manager.get_enemy_base_location())
-
+func _physics_process(_delta: float) -> void:
 	if enemy_queue.size() > 0 and not in_combat:
 		in_combat = true
 		attack()
@@ -47,6 +29,7 @@ func _physics_process(delta: float) -> void:
 	if current_health <= 0:
 		await get_tree().create_timer(.5).timeout
 		die()
+
 
 func die() -> void:
 	#TODO: Give money to player
@@ -76,7 +59,5 @@ func take_damage(damage: int) -> bool:
 	return is_alive
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group(attack_target):
-		navgationAgent2D.set_target_position(body.global_position)
-		targeted_enemy = true
-		enemy_queue.append(body)
+	if body.get_parent().is_in_group(attack_target):
+		enemy_queue.append(body.get_parent())
