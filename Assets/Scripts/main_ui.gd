72315c31ff31a362f300b0e44dbc4dc81869b645
EyeLax <eyelax@outlook.com>
2024-10-05 21:57:42 +0200

    I forgot to merge

--- Assets/Scripts/main_ui.gd
+++ Assets/Scripts/main_ui.gd
@@ -3,10 +3,18 @@ extends Control
 @onready var bosses_left: Label = $BoxContainer/BossesLeft
 @onready var attempt_tracker: Label = $BoxContainer/AttemptTracker
 @onready var ally_tracker: Label = $BoxContainer/AllyTracker
+@onready var gold: Label = $BoxContainer/Gold
 
 func _ready():
 	GLOBALVARIABLES.main_ui = self
 
+func _process(_delta):
+	bosses_left.text = "Bosses Left: " + str(GLOBALVARIABLES.bosses_left)
+	attempt_tracker.text = "Attampt Nr: " + str(GLOBALVARIABLES.round_counter)
+	ally_tracker.text = "Allys Left: " + str(GLOBALVARIABLES.ally_count)
+	gold.text = "Gold: " + str(GLOBALVARIABLES.player_resource)
+
+	
 func _on_start_wave_button_button_up() -> void:
 	GLOBALVARIABLES.game_manager.start_wave()
 
@@ -17,7 +25,3 @@ func set_upgrade_panel_visibility(toggle : bool):
 	%UpgradePanel.visible = toggle
 	%UpgradePanel.update_gold_label()
 	
-func update_player_labls():
-	bosses_left.text = str("Bosses Left: " + GLOBALVARIABLES.bosses_left)
-	attempt_tracker.text = str("Attampt Nr: " + GLOBALVARIABLES.attempt_tracker)
-	ally_tracker.text = str("Allys Left: " + GLOBALVARIABLES.ally_tracker)
