433c98071ac33370172350a5fb631195dda6db2e
EyeLax <eyelax@outlook.com>
2024-10-05 15:41:57 +0200

    Upgrade UI

--- Assets/Scripts/game_manager.gd
+++ Assets/Scripts/game_manager.gd
@@ -1,7 +1,23 @@
 extends Node2D
 
+var ally_count = 0
+
 @export var distance_to_enemy = 20
+
+func _ready() -> void:
+	ally_count = get_node("Creatures:").get_child_count()
+
+func _process(_delta):
+	if ally_count <= 0:
+		lose()
 
 func get_enemy_base_location() -> Vector2:
 	return get_node("EnemyBase").global_position
 
+func win() -> void:
+	print("You win!")
+	get_tree().reload_current_scene()
+
+func lose() -> void:
+	print("You lose!")
+	get_tree().quit()
---
+++ Assets/Scripts/upgrade_panel.gd
@@ -0,0 +1,11 @@
+extends Control
+
+@onready var grid_container: GridContainer = $PanelContainer/GridContainer
+
+func _on_hp_upgrade_pressed() -> void:
+	#TODO: Connect to main 
+	var add_hp: int  = 10
+
+func _on_ally_upgrade_pressed() -> void:
+	#TODO: Connect to main
+	var add_to_ally_count: int = 1
