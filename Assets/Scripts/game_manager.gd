f60ed000cd4429cfb1919e108067c88e13dd38f3
EyeLax <eyelax@outlook.com>
2024-10-05 17:41:44 +0200

    Upgrades panel yes

--- Assets/Scripts/game_manager.gd
+++ Assets/Scripts/game_manager.gd
@@ -1,37 +1,47 @@
 extends Node2D
 
-var ally_count = 0
+var ongoing_wave = false
 
 @export var distance_to_enemy = 20
 @onready var main_ui : Control = %MainUI
 
 func _ready():
 	GLOBALVARIABLES.game_manager = self
-
-func _ready() -> void:
-	ally_count = get_node("Creatures:").get_child_count()
+	if GLOBALVARIABLES.round_counter >= 1:
+		main_ui.set_upgrade_panel_visibility(true)
+		main_ui.set_start_wave_button_visibility(false)
+	else:
+		main_ui.set_upgrade_panel_visibility(false)
+		main_ui.set_start_wave_button_visibility(true)
 
 func _process(_delta):
-	if ally_count <= 0:
-		lose()
+	if ongoing_wave:
+		if GLOBALVARIABLES.ally_count <= 0:
+			lose()
 
 func get_enemy_base_location() -> Vector2:
 	return get_node("EnemyBase").global_position
 
 func win() -> void:
-	print("You win!")
-	get_tree().reload_current_scene()
+	get_tree().quit()
+	
 
 func lose() -> void:
 	print("You lose!")
-	get_tree().quit()
+	GLOBALVARIABLES.round_counter += 1
+	get_tree().reload_current_scene()
+	
 func start_wave() -> void:
 	main_ui.set_start_wave_button_visibility(false)
+	ongoing_wave = true
 	spawn_creatures()
 
 func spawn_creatures() -> void:
 	for i in range(0,GLOBALVARIABLES.player_creature_count):
 		print(%spawnArea.shape.get_size())
+		GLOBALVARIABLES.ally_count += 1
 		#var top_left = %spawnArea.global_position
 		#var bottom_right = %spawnArea.global_position.x - %spawnArea.transform.
-		
+
+func _on_bass_range_body_entered(_body: Node2D) -> void:
+	win()
--- Assets/Scripts/main_ui.gd
+++ Assets/Scripts/main_ui.gd
@@ -1,7 +1,13 @@
 extends Control
 
+func _ready():
+	GLOBALVARIABLES.main_ui = self
+
 func _on_start_wave_button_button_up() -> void:
 	GLOBALVARIABLES.game_manager.start_wave()
 
 func set_start_wave_button_visibility(toggle : bool):
 	%StartWaveButton.visible = toggle
+
+func set_upgrade_panel_visibility(toggle : bool):
+	%UpgradePanel.visible = toggle
